83356cbd6924f2fc15870f46069d18d519f73608
milinkorath <milinkorath@gmail.com>
2015-05-11 18:39:31 +0530

    first commit

---
+++ Project1/src/test/impel/Hello.java
@@ -0,0 +1,24 @@
+//*********************************************************************
+//
+//                        Copyright (C) 2015
+//                      Carnegie Learning Inc.
+//
+//                       All Rights Reserved.
+//
+//This program is the subject of trade secrets and intellectual
+//property rights owned by Carnegie Learning.
+//
+//This legend must continue to appear in the source code despite
+//modifications or enhancements by any party.
+//
+//*********************************************************************
+
+/*
+  $Id$
+  $Name$
+*/
+package test.impel;
+
+public class Hello {
+
+}
